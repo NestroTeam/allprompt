5a924ad543782846558f4342033a33d25ff46bfa
NixonXC <81410474+NixonXC@users.noreply.github.com>
2022-01-18 09:09:05 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 document.getElementById("we1").onclick = function ()
 {
     window.location.href="pages/counter.html"
-}
+};
+document.getElementById("we2").onclick = function ()
+{
+    window.location.href="pages/time.html"
+};
